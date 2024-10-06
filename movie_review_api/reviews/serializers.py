6c5cdd938d8d28fdf175c0a00eb8a875f03be8e3
BAIBARS7 <159480461+BAIBARS7@users.noreply.github.com>
2024-10-06 22:05:29 +0300

    Update 1 serializers.py

--- movie_review_api/reviews/serializers.py
+++ movie_review_api/reviews/serializers.py
@@ -5,7 +5,13 @@ from .models import Review, Profile, Comment
 class ReviewSerializer(serializers.ModelSerializer):
     class Meta:
         model = Review
-        fields = '__all__'
+        fields = ['id', 'movie_title', 'review_content', 'rating', 'user', 'created_date']
+        read_only_fields = ['user']
+
+    def validate_rating(self, value):
+        if value < 1 or value > 5:
+            raise serializers.ValidationError("Rating must be between 1 and 5.")
+        return value
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
